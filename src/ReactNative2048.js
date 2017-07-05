acaffacc91d847015e6dbb2d4478ac78a33aaff4
gabrielsclimaco <gabrielsclimaco@gmail.com>
2017-07-05 00:22:52 -0300

    [UI] Creates game header front end

--- src/ReactNative2048.js
+++ src/ReactNative2048.js
@@ -10,8 +10,25 @@ import Style from './style/Style'
 export default class ReactNative2048 extends Component {
   render() {
     return (
-      <View style={Style.homeTitle}>
-        <Text>hello world</Text>
+      <View style={Style.homeScreen}>
+        <View style={Style.homeTitle}>
+          <Text style={Style.title}>2048</Text>
+          <View style={Style.scores}>
+            <View style={Style.scoreBoard}>
+              <Text style={Style.scoreTitle}>pontos</Text>
+              <Text style={Style.scoreValue}>184</Text>
+            </View>
+            <View style={Style.scoreBoard}>
+              <Text style={Style.scoreTitle}>recorde</Text>
+              <Text style={Style.scoreValue}>2048</Text>
+            </View>
+          </View>
+        </View>
+        <View style={Style.homeGame}>
+          <View style={Style.board}>
+
+          </View>
+        </View>
       </View>
     );
   }
--- src/style/Style.js
+++ src/style/Style.js
@@ -1,11 +1,73 @@
 import { StyleSheet } from 'react-native'
+import {
+  responsiveHeight,
+  responsiveWidth,
+  responsiveFontSize
+} from 'react-native-responsive-dimensions'
+
 
 const Style = StyleSheet.create({
-  homeTitle: {
+
+  homeScreen: {
     flex: 1,
+    backgroundColor: '#faf8ef'
+  },
+
+  homeTitle: {
+    flex: 2,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    // backgroundColor: 'green',
+    padding: 25
+  },
+
+  homeGame: {
+    flex: 8,
     justifyContent: 'center',
+    // backgroundColor: 'red',
     alignItems: 'center'
+  },
+
+  board: {
+    backgroundColor: '#bbada0'
+  },
+
+  scores: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    width: responsiveWidth(45)
+  },
+
+  scoreBoard: {
+    height: responsiveHeight(10),
+    minWidth: responsiveWidth(20),
+    backgroundColor: '#bbada0',
+    padding: 10,
+    borderRadius: 10,
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+
+  title: {
+    color: '#776e65',
+    fontSize: responsiveFontSize(5),
+    fontWeight: 'bold'
+  },
+
+  scoreTitle: {
+    color: '#eee4da',
+    fontWeight: 'bold',
+    fontSize: responsiveFontSize(2)
+  },
+
+  scoreValue: {
+    color: '#faf8ef',
+    fontWeight: 'bold',
+    fontSize: responsiveFontSize(3)
   }
+
 })
 
 export default Style

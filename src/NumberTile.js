2df1cc947edb009f1880506216c822bc463df24b
gabrielsclimaco <gabrielsclimaco@gmail.com>
2017-07-08 10:02:11 -0300

    [UI] Adds spring animation to number tile on component mounting

--- src/NumberTile.js
+++ src/NumberTile.js
@@ -1,34 +1,43 @@
 import React, { PureComponent } from 'react';
 import {
   Text,
+  View,
   Animated,
 } from 'react-native';
 
 import Style from './style/Style';
 
 export default class NumberTile extends PureComponent {
-  // constructor () {
-  //   super()
-  //   this.springValue = new Animated.Value(0.3)
-  // }
-  //
-  // spring () {
-  //   this.springValue.setValue(0.3)
-  //   Animated.spring(
-  //     this.springValue,
-  //     {
-  //       toValue: 1,
-  //       friction: 1
-  //     }
-  //   ).start()
-  // }
+  constructor() {
+    super();
+    this.springValue = new Animated.Value(0);
+  }
+
+  componentDidMount() {
+    this.spring();
+  }
+
+  spring() {
+    this.springValue.setValue(0);
+    Animated.spring(
+      this.springValue,
+      {
+        toValue: 1,
+        friction: 4,
+      },
+    ).start();
+  }
 
   render() {
     return (
-      <Animated.View style={Style['tile' + this.props.numberOfTile]}>
-        <Text style={Style['text' + this.props.numberOfTile]}>
-          {this.props.numberOfTile}
-        </Text>
+      <Animated.View
+        style={{ flex: 1, transform: [{ scale: this.springValue }] }}
+      >
+        <View style={Style['tile' + this.props.numberOfTile]}>
+          <Text style={Style['text' + this.props.numberOfTile]}>
+            {this.props.numberOfTile}
+          </Text>
+        </View>
       </Animated.View>
     );
   }
